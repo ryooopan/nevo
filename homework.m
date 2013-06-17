053cd68dfdd1a6da29ecb721be86cf3f34d92208
Ryo Suzuki <ryooopan@googlemail.com>
2013-06-18 00:57:43 +0900

    fix question 2 and add question 4

--- homework.m
+++ homework.m
@@ -6,20 +6,23 @@ diary off
 load ps2.mat
 load iv.mat
 
-s_0 = []
+
+
+% Question. 1
+
+s_o = []
 
 n_brand = 24
 n_market = 94
 
-for k = 1: n_market 
-  sum = 0
-  for i = k: k - 1 + n_brand
-    sum = sum + s_jt(i, :)
-  end
-  s_0 = [ s_0; ones(n_brand, 1) * (1 - sum)]
+
+a = sum( s_jt( reshape( 1: size(s_jt, 1), [n_brand n_market] ) ) )
+
+for k = 1: n_market
+  s_o = [ s_o; ones(n_brand, 1) * ( 1 - a(k) ) ]
 end
 
-y = log(s_jt) - log(s_0)
+y = log(s_jt) - log(s_o)
 x = x2
 z = [x iv]
 
@@ -32,9 +35,79 @@ disp("Question 1")
 
 diary off
 
-index_1 = find(x(:, 4) > 0)
-index_0 = find(x(;, 4) < 1)
 
-g_0 = s_jt(index_0, :)
-g_1 = s_jt(index_1, :)
+
+
+% Question. 2
+
+index_1 = find(x(:, 4) > 0)
+index_0 = find(x(:, 4) < 1)
+
+s_0 = s_jt
+s_1 = s_jt
+
+s_0(index_1, :) = 0
+s_1(index_0, :) = 0
+
+a_0 = sum( s_0( reshape( 1: size(s_0, 1), [n_brand n_market] ) ) )'
+a_1 = sum( s_1( reshape( 1: size(s_1, 1), [n_brand n_market] ) ) )'
+
+
+s_g_0 = kron( a_0, ones(n_brand, 1) )
+s_g_1 = kron( a_1, ones(n_brand, 1) )
+
+s_0 = s_0 ./ s_g_0 
+s_1 = s_1 ./ s_g_1
+
+s_g = s_0 + s_1
+
+y = log(s_jt) - log(s_o)
+x = [x s_g]
+z = [x iv]
+
+diary on
+
+disp(" ")
+disp("----------")
+disp("Question 2")
+
+[beta, sigma] = ols (y, z)
+
+diary off
+
+
+
+
+% Question. 3 
+
+
+
+
+
+% Question. 4
+
+zeta = rand(n_brand, 1)
+error = kron( ones(n_market, 1), zeta )
+
+
+beta = [-3.0; 10.0; 0.1; 0.05]
+x = x2
+
+y = x * beta + error
+
+beta_hat = ols (y, x)
+
+
+diary on
+
+disp(" ")
+disp("----------")
+disp("Question 4")
+
+zeta
+beta
+beta_hat
+
+diary off
+
 
